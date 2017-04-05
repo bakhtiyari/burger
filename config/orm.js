480e39728fa63fb45f51b31d6679e0ab21235942
bakhtiyari <bahman16>
2017-04-05 16:22:17 -0400

    First Version

---
+++ config/orm.js
@@ -0,0 +1,63 @@
+// Import MySQL connection.
+var connection = require('../config/connection.js');
+
+// Helper function for SQL syntax.
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push('?')
+    }
+
+    return arr.toString();
+}
+
+// Helper function for SQL syntax.
+function objToSql(ob) {
+
+    var arr = [];
+
+    for (var key in ob) {
+        arr.push(key + '=' + ob[key]);
+    }
+
+    return arr.toString();
+}
+
+// Object for all our SQL statement functions.
+var orm = {
+    all: function(tableInput, cb) {
+        var queryString = 'SELECT * FROM ' + tableInput + ';';
+
+        console.log(queryString)
+
+        connection.query(queryString, function(err, result) {
+
+            console.log(result);
+            cb(result);
+        });
+    },
+
+    create: function(table, cols, vals, cb) {
+        var queryString = 'INSERT INTO ' + table + ' (' + cols.toString() + ') ' + 'VALUES (' + printQuestionMarks(vals.length) + ') ';
+
+        console.log(queryString)
+
+        connection.query(queryString, vals, function(err, result) {
+            cb(result);
+        });
+    },
+
+    update: function(table, objColVals, condition, cb) {
+        var queryString = 'UPDATE ' + table + ' SET ' + objToSql(objColVals) + ' WHERE ' + condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, function(err, result) {
+            cb(result);
+        });
+    }
+};
+
+// Export the orm object for the model (burger.js)
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("Chef Burger");
+INSERT INTO burgers (burger_name) VALUES ("Akropolis Burger");
+INSERT INTO burgers (burger_name) VALUES ("Mushroom Burger");
+
+
